891acda64927249d25ea13c004f6aec3e1bb1a3f
Noland <noland.germain@gmail.com>
2018-10-02 18:31:10 -0700

    Peachy

--- src/lib/components/FileUploader.js
+++ src/lib/components/FileUploader.js
@@ -34,7 +34,10 @@ const styles = theme => ({
     backgroundColor: 'rgba(0,0,0,.1)',
   },
   reject: {
-    backgroundColor: red[500],
+  },
+  active: {
+  },
+  accept: {
   },
   formControl: {
 
@@ -69,14 +72,14 @@ const previewStyles = theme => ({
 });
 
 class PreviewFileList extends React.Component {
-  renderFile = ({ loaded, src, file, uri }) => {
-    const { classes } = this.props;
+  renderFile = ({ loaded, src, file, id }) => {
+    const { classes, removeFile } = this.props;
+
     return(
-      <Card key={uri} className={classes.card}>
+      <Card key={id} className={classes.card}>
         <CardActionArea className={classes.actionArea}>
           {loaded
             ? <CardMedia image={src} title={`Preview for ${file.name}`} className={classes.media} />
-            // ? <img src={src} alt={`Preview for ${file.name}`} className={classes.mediaImg} />
             : <CircularProgress className={classes.progress} size={50} />
           }
           <CardContent>
@@ -84,7 +87,7 @@ class PreviewFileList extends React.Component {
           </CardContent>
         </CardActionArea>
         <CardActions className={classes.actions}>
-          <Button style={{ color: red[500] }} onClick={() => this.removeFile(file)} size="small">
+          <Button style={{ color: red[500] }} onClick={() => removeFile(file)} size="small">
             <DeleteIcon /><span>Remove</span>
           </Button>
         </CardActions>
@@ -106,6 +109,7 @@ class PreviewFileList extends React.Component {
 PreviewFileList.propTypes = {
   classes: PropTypes.object.isRequired,
   files: PropTypes.object.isRequired,
+  removeFile: PropTypes.func.isRequired,
 };
 PreviewFileList = withStyles(previewStyles)(PreviewFileList);
 
@@ -115,34 +119,63 @@ class FileUploader extends React.Component {
     super(props);
     this.state = {
       previews: {},
+      inputKey: Math.random().toString(36),
     };
   }
 
-  onDrop = (files) => {
+  removeFile = (file) => {
     const { previews } = this.state;
-    const { onChange } = this.props;
-    onChange(files[0]);
+    const { onChange, multiple } = this.props;
+    const id = this.getId(file);
+
+    delete previews[id];
+    this.setState({ previews });
+
+    let value = _.map(previews, p => p.file);
+    if(!multiple) value = value[0];
+    onChange(value);
+  }
+
+  getId = file => file.name
+
+  onDrop = (files, supressUpstream = false) => {
+    const { previews } = this.state;
+    const { onChange, multiple } = this.props;
+    const value = multiple ? files : files[0];
+    if(!supressUpstream) onChange(value);
 
     let modifiedPreviews = false;
+
+    _.forEach(previews, (preview) => {
+      const file = files.find(f => this.getId(f) === this.getId(preview.id));
+      if(!file) {
+        delete previews[preview.id];
+        modifiedPreviews = true;
+      }
+    });
+
     files.forEach((file) => {
-      const uri = file.preview;
-      const preview = previews[uri];
+      const id = this.getId(file);
+      const preview = previews[id];
 
       if(!preview) {
-        const reader = new window.FileReader();
         const newPreview = {
-          uri,
+          id,
           file,
-          loaded: false,
-          reader,
+          src: file.src,
+          loaded: file.loaded,
         };
-        reader.onload = (e) => {
-          newPreview.src = e.target.result;
-          newPreview.loaded = true;
-          this.forceUpdate();
-        };
-        reader.readAsDataURL(file);
-        previews[uri] = newPreview;
+
+        if(!newPreview.loaded) {
+          const reader = new window.FileReader();
+          reader.onload = (e) => {
+            newPreview.src = e.target.result;
+            newPreview.loaded = true;
+            this.forceUpdate();
+          };
+          reader.readAsDataURL(file);
+        }
+        previews[id] = newPreview;
         modifiedPreviews = true;
       }
     });
@@ -150,16 +183,50 @@ class FileUploader extends React.Component {
     if(modifiedPreviews) this.setState({ previews });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { value } = this.props;
+
+    if(!_.isEqual(prevProps.value, value)) {
+      this.setState({ inputKey: Math.random().toString(36) }); // eslint-disable-line
+      const files = this.convertInputValueToFiles(value);
+      this.onDrop(files, true);
+    }
+  }
+
+  convertInputValueToFiles = (value) => {
+    const files = [];
+
+    if(!Array.isArray(value)) value = [value];
+
+    value.forEach((f) => {
+      if(!f) return;
+
+      if(!f.filename) {
+        console.log(value);
+      }
+
+      const file = {
+        name: f.filename,
+        src: f.location,
+        loaded: true,
+      };
+      files.push(file);
+    });
+
+    return files;
+  }
+
   render() {
-    const { previews } = this.state;
-    const { classes, label, helperText, error, disabled, ...rest } = this.props;
+    const { previews, inputKey } = this.state;
+    const { classes, label, helperText, error, disabled, multiple, ...rest } = this.props;
+    const { value } = this.props;
+
 
     return (
       <FormControl className={classes.formControl} error={error} disabled={disabled}>
         <Typography variant="body1">{label}</Typography>
         <Dropzone
           onDrop={this.onDrop}
-          accept="image/*"
           disabled={disabled}
           acceptClassName="accept"
           activeClassName="active"
@@ -167,6 +234,8 @@ class FileUploader extends React.Component {
           disabledClassName="disabled"
           rejectClassName="reject"
           maxSize={20e6}
+          multiple={multiple}
+          inputProps={{ key: inputKey }}
           {...rest}
         >
           <UploadIcon style={{ fontSize: 50 }} color="primary" />
@@ -175,7 +244,7 @@ class FileUploader extends React.Component {
             <FolderIcon /><span>Browse for files</span>
           </Button>
         </Dropzone>
-        <PreviewFileList files={previews} />
+        <PreviewFileList files={previews} removeFile={this.removeFile} />
         <FormHelperText>{helperText}</FormHelperText>
       </FormControl>
     );
@@ -190,12 +259,14 @@ FileUploader.propTypes = {
   // value: PropTypes.any,
   onChange: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
+  multiple: PropTypes.bool,
 };
 FileUploader.defaultProps = {
   helperText: undefined,
   error: undefined,
   disabled: undefined,
   value: undefined,
+  multiple: false,
 };
 
 const FileUploaderEnhanced = withFormFields(withStyles(styles)(FileUploader));
