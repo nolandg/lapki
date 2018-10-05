b603a43c79fb6be11340b4e4fb333fe6945bf447
Noland <noland.germain@gmail.com>
2018-10-04 22:12:36 -0700

    Single

--- src/lib/components/Mutator.js
+++ src/lib/components/Mutator.js
@@ -200,9 +200,7 @@ class Mutator extends Component {
     this.clearErrors();
 
     const doc = assembleDoc();
-    console.log('assembled doc: ', doc);
     const castDoc = await validateDoc(doc);
-    console.log('cast doc: ', doc);
 
     return castDoc;
   }
