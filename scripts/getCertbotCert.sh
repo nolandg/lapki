b26211bf1d5b9ba28d7a72b5f30dcc213d2ade1b
Noland <noland.germain@gmail.com>
2019-04-13 11:27:38 -0700

    Peachy

---
+++ scripts/getCertbotCert.sh
@@ -0,0 +1,10 @@
+certbot certonly \
+  --dns-digitalocean \
+  --dns-digitalocean-credentials ~/digitalocean.ini \
+  -d powellriver.ca
+  -d www.powellriver.ca
+  -d api.powellriver.ca
+  -d static.powellriver.ca
+  -d dev.powellriver.ca
+  -d dev.api.powellriver.ca
+  -d dev.static.powellriver.ca
